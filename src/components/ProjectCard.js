41946a3f7a11fa531de6ff0674691f27bbded4b3
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2023-03-24 16:36:18 -0300

    feat: better cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,13 +6,11 @@ function ProjectCard(props) {
 
     return (
           <div className="ProjectCard">
-            <a href={webLink} target="_blank" rel="noopener noreferrer">
-              <img src={imageSrc} alt={title} className="ProjectCard__image" />
-              <div className="ProjectCard__title">{title}</div>
-            </a>
-            <a href={codeLink} target="_blank" rel="noopener noreferrer" className="ProjectCard__code-link">
-              Source Code
-            </a>
+              <a href={webLink} target="_blank"><img src={imageSrc} alt={title} className="ProjectCard__image" /></a>
+              <div className="ProjectCard__bar">
+                <a href={codeLink} target="_blank"><div className="ProjectCard__title">{title}</div></a>
+                <a href={codeLink} target="_blank" rel="noopener noreferrer" className="ProjectCard__code-link">&lt;/&gt;</a>
+              </div>
           </div>
         );
 }
