5567a7758c23768748a2c496a9860df2a1225cef
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2023-07-21 15:37:43 -0300

    feat: improved project card and added SocialHub description

--- src/components/DescriptionModal/DescriptionModal.js
+++ src/components/DescriptionModal/DescriptionModal.js
@@ -1,48 +1,75 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
-import './styles.css';
+import "./styles.css";
 
 Modal.setAppElement("#root");
 
-const ProjectDescriptionModal = ({ isOpen, onClose, title, imageSrc, description, demoLink, sourceLink }) => {
-  const [content, setContent] = useState("");
+const ProjectDescriptionModal = ({
+    isOpen,
+    onClose,
+    title,
+    imageSrc,
+    description,
+    demoLink,
+    sourceLink,
+    techTags,
+}) => {
+    const [content, setContent] = useState("");
 
-  const handleInputChange = (e) => {
-    setContent(e.target.value);
-  };
+    const handleInputChange = (e) => {
+        setContent(e.target.value);
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Aquí puedes realizar alguna acción con el contenido ingresado en el formulario
-    onClose();
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Aquí puedes realizar alguna acción con el contenido ingresado en el formulario
+        onClose();
+    };
 
-  return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      contentLabel="Project description"
-    >
-      <div className="TopView">
-        <h2>{title}</h2>
-        <button className="CrossButton" onClick={onClose}>X</button>
-      </div>
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onClose}
+            contentLabel="Project description">
+            <div className="TopView">
+                <h2>{title}</h2>
+                <button className="CrossButton" onClick={onClose}>
+                    X
+                </button>
+            </div>
 
-      <div className="MainContainer">
-        <div className="LeftSide">
-          <img src={imageSrc} className="ImageBox"></img>
-        </div>
-        <div className="DescriptionContainer">
-          <h3>Description:</h3>
-          <p>{description}</p>
-          <a href={demoLink} className="Link">Demo</a>
-          <a href={sourceLink} className="Link">Source</a>
-        </div>
-      </div>
-
-    </Modal>
-  );
+            <div className="MainContainer">
+                <div className="LeftSide">
+                    <img src={imageSrc} className="ImageBox"></img>
+                </div>
+                <div className="DescriptionContainer">
+                    <h3>Description:</h3>
+                    <p>{description}</p>
+                    {techTags && (
+                        <div className="TagContainer">
+                            {techTags.split(";").map((item, index, arr) => (
+                                <span className="tag">{item}</span>
+                            ))}
+                        </div>
+                    )}
+                    <a
+                        href={demoLink}
+                        target="_blank"
+                        className="Link"
+                        rel="noreferrer">
+                        Demo
+                    </a>
+                    <a
+                        href={sourceLink}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="Link">
+                        Source
+                    </a>
+                </div>
+            </div>
+        </Modal>
+    );
 };
 
 export default ProjectDescriptionModal;
- 
--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,41 +1,93 @@
-import React, {useState} from 'react';
-import './styles.css';
+import React, { useState } from "react";
+import "./styles.css";
 import DescriptionModal from "../DescriptionModal/DescriptionModal.js";
 
 function ProjectCard(props) {
-    const { title, imageSrc, webLink, sourceLink, description } = props;
+    const {
+        title,
+        imageSrc,
+        webLink,
+        sourceLink,
+        description,
+        demoLink,
+        techTags,
+    } = props;
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const openModal = () => {
-      console.log("ClICK");
-      setIsModalOpen(true);
-    }
+        console.log("ClICK");
+        setIsModalOpen(true);
+    };
 
     const closeModal = () => {
-      console.log("CLOSE");
-      setIsModalOpen(false);
-    }
+        console.log("CLOSE");
+        setIsModalOpen(false);
+    };
 
     return (
-          <div className="ProjectCard">
-              <a href={webLink} target="_blank"><img src={imageSrc} alt={title} className="ProjectCard__image" /></a>
-    <div className="ProjectCard__bar">
-      {webLink != null && <a href={webLink} target="_blank"><div className="ProjectCard__title">{title}</div></a>}
-      {webLink == null && <div className="ProjectCard__title" >{title}</div>}
-    <div className="ProjectCard__bar">
-      {description && <button onClick={openModal} className="ProjectCard__code-link">+info</button>}
-      {!description && <div className="ProjectCard__code-link Disabled">+info</div>}
-      
-      {sourceLink && <a href={sourceLink} target="_blank" rel="noopener noreferrer" className="ProjectCard__code-link">&lt;/&gt;</a>}
-      {!sourceLink && <div className="ProjectCard__code-link Disabled">&lt;/&gt;</div>}
+        <div className="ProjectCard">
+            <a href={webLink} target="_blank" rel="noreferrer">
+                <img
+                    src={imageSrc}
+                    alt={title}
+                    className="ProjectCard__image"
+                />
+            </a>
+            <div className="ProjectCard__bar">
+                {webLink != null && (
+                    <a href={webLink} target="_blank" rel="noreferrer">
+                        <div className="ProjectCard__title">{title}</div>
+                    </a>
+                )}
+                {webLink == null && (
+                    <div className="ProjectCard__title">{title}</div>
+                )}
+                <div className="ProjectCard__bar">
+                    {description && (
+                        <button
+                            onClick={openModal}
+                            className="ProjectCard__code-link">
+                            +info
+                        </button>
+                    )}
+                    {!description && (
+                        <div className="ProjectCard__code-link Disabled">
+                            +info
+                        </div>
+                    )}
+
+                    {sourceLink && (
+                        <a
+                            href={sourceLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="ProjectCard__code-link">
+                            &lt;/&gt;
+                        </a>
+                    )}
+                    {!sourceLink && (
+                        <div className="ProjectCard__code-link Disabled">
+                            &lt;/&gt;
+                        </div>
+                    )}
                 </div>
-              </div>
-        
-      {description && isModalOpen && <DescriptionModal isOpen={true} onClose={closeModal} title={title} imageSrc={imageSrc} description={description}/>}
-          </div>
-        );
+            </div>
+
+            {description && isModalOpen && (
+                <DescriptionModal
+                    isOpen={true}
+                    onClose={closeModal}
+                    title={title}
+                    imageSrc={imageSrc}
+                    description={description}
+                    demoLink={demoLink}
+                    sourceLink={sourceLink}
+                    techTags={techTags}
+                />
+            )}
+        </div>
+    );
 }
 
 export default ProjectCard;
-
