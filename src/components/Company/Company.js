902a258b3777ce2af3dc0cd5a67519f222597a37
Gonzalo Del Gaudio <gonzalodelgaudio@gmail.com>
2023-07-24 21:35:31 -0300

    style: improved companies section on small devices

--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -5,9 +5,7 @@ const Company = ({ name, imageSrc }) => {
     return (
         <div className="Company" style={{ textAlign: "center" }}>
             <img src={imageSrc} alt={name} className="Image" style={{}} />
-            <h3 style={{ margin: "0", padding: "0", fontSize: "24px" }}>
-                {name}
-            </h3>
+            <h3>{name}</h3>
         </div>
     );
 };
